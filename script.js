9b745e0c5844824d3a1e4461a6745ae117647708
Himmu9131 <114733178+Himmu9131@users.noreply.github.com>
2025-01-07 16:00:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const inputCode= document.querySelectorAll(".code");
+inputCode.forEach((input,index)=>{
+	input.addEventListener("input",(e)=>{
+		const value=e.target.value;
+		if(value&&index<inputCode.length-1){
+			inputCode[index+1].focus();
+		}
+	});
+	input.addEventListener("keydown",(e)=>{
+		if(e.key==="Backspace") {if(!e.target.value && index>0){
+			inputCode[index-1].focus();
+		}}
+	});
+});
